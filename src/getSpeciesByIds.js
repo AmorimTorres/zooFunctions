093cf9c8183ba3006814b7f0bdacc374d0dc5de5
André <amorimtorresandre@gmail.com>
2022-01-06 16:18:04 -0300

    requisito 1 check

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  const speciesFinding = [];
+  ids.forEach((id) => speciesFinding.push(species.find((specie) => specie.id === id)));
+  return speciesFinding;
 }
+
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
