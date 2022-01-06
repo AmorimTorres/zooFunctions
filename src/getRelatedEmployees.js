61434b5cc69ea2688214a0e334d37558efc744b6
André <amorimtorresandre@gmail.com>
2022-01-06 20:00:24 -0300

    requisito 4 check

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
+const { employees } = data;
 
 function isManager(id) {
-  // seu código aqui
+  const employeesList = employees.some((employee) => employee.managers.find((item) => item === id));
+  return employeesList;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const obj = employees.filter((pers) => pers.managers.find((pers1) => pers1 === managerId));
+  if (isManager(managerId)) {
+    return obj.map((emp) => `${emp.firstName} ${emp.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2');
+
+console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = { isManager, getRelatedEmployees };
