bef7e651e490990b9a17bcf12a1219c518ada2e3
André <amorimtorresandre@gmail.com>
2022-01-11 14:05:00 -0300

    requisito 5 check

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,11 +3,22 @@ const { species } = data;
 
 function countAnimals(animal) {
   if (!animal) {
-    return species.reduce((acc, specie) => { acc[specie.name] = specie.residents.length;
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
       return acc;
-  }, {});
+    }, {});
+  } if (animal.sex) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.filter((sexo) => sexo.sex === animal.sex).length;
+      return acc;
+    }, {})[animal.specie];
+  }
+  return species.reduce((acc, specie) => {
+    acc[specie.name] = specie.residents.length;
+    return acc;
+  }, {})[animal.specie];
 }
 
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'snakes', sex: 'female' }));
 
 module.exports = countAnimals;
