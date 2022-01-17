2f3253c43be0fc8acc3810317694396f58483ed7
André <amorimtorresandre@gmail.com>
2022-01-17 12:10:06 -0300

    requisito 1 refatorado :)

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,11 +3,12 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  const speciesFinding = [];
-  ids.forEach((id) => speciesFinding.push(species.find((specie) => specie.id === id)));
-  return speciesFinding;
+  return ids.reduce((acc, specie) => {
+    acc.push(species.find((findSpecie) => findSpecie.id === specie));
+    return acc;
+  }, []);
 }
 
-getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', '78460a91-f4da-4dea-a469-86fd2b8ccc84');
+getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', '0938aa23-f153-4937-9f88-4858b24d6bce');
 
 module.exports = getSpeciesByIds;
