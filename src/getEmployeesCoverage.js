b351cd38759b0b00a928b7bb2babb45f89a5c3d6
André <amorimtorresandre@gmail.com>
2022-01-17 10:53:10 -0300

    requisito 10 check - IHUU

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function findEmployee(obj) {
+  return employees
+    .find((emp) => (emp.id === obj.id || emp.firstName === obj.name || emp.lastName === obj.name));
 }
+
+function findSpecies(obj) {
+  const employeeResp = findEmployee(obj).responsibleFor;
+  return species.filter((specie) => employeeResp.includes(specie.id));
+}
+
+function getEmployeesCoverage(obj) {
+  if (!obj) {
+    return employees.map((employee) => (
+      {
+        id: employee.id,
+        fullName: `${employee.firstName} ${employee.lastName}`,
+        species: findSpecies(employee).map((specie) => specie.name),
+        locations: findSpecies(employee).map((specie) => specie.location),
+      }
+    ));
+  }
+  if (!findEmployee(obj)) throw new Error('Informações inválidas');
+  const { id, firstName, lastName } = findEmployee(obj);
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: findSpecies(obj).map((specie) => specie.name),
+    locations: findSpecies(obj).map((specie) => specie.location),
+  };
+}
+
+getEmployeesCoverage();
+
+// requisito feito com referências nas resoluções de Tabata, Kamyla, Gabriel resende e Carol So e apoio do Thalles Carneiro nas monitorias Summer.
 
 module.exports = getEmployeesCoverage;
